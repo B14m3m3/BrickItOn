df88c8a5da51996278fcbea52f2a81af12f5e6f2
Morten <bitbucket@moddi.dk>
2018-04-14 10:23:23 +0200

    Implemented config-file

--- README.md
+++ README.md
@@ -3,7 +3,18 @@
 # Setup
 Here is the guide for installing required packages.
 
+### Config
+Copy the config sample and put your settings into it:
+```
+cp config_sample.py config.py
+nano config.py
+```
+
+
 ### Webcam
+Install OpenCV:
 ```
 pip install opencv-python
 ```
+
+Now setup environment variable PYTHONPATH to point to the root dir of this project.
--- Webcam/webcam.py
+++ Webcam/webcam.py
@@ -1,13 +1,18 @@
 import cv2
+from config import *
+
 
 class Webcam:
+    def __init__(self):
+        print("Using webcam number", Config.webcam)
+
     def takePicture(self):
-        cam = cv2.VideoCapture(0)
+        cam = cv2.VideoCapture(Config.webcam)
         retval, frame = cam.read()
         if retval != True:
             raise ValueError("Can't read frame")
 
-        cv2.imwrite('img2.png', frame)
+        # cv2.imwrite('img2.png', frame)
         cv2.imshow("img1", frame)
         cv2.waitKey()
         return frame
